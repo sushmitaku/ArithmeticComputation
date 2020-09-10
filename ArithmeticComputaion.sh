3f01171b3024fe1c0e697ab4b6e4d9c700134c77
sushmitaku <ksushmita1999@gmail.com>
2020-09-10 11:08:37 +0530

    Above All Arithmetic Expression Calculated is Stored in Dictionary

--- ArithmeticComputaion.sh
+++ ArithmeticComputaion.sh
@@ -1,15 +1,21 @@
 # program to take 3 inputs 
+
+declare -A Dictionary
 read -p "enter a" a
 read -p "enter b" b
 read -p "enter c" c
-echo "$a  $b  $c "
-#result1=$(( $a + $b *$c ))
-#echo $result1
+i=1
+result1=$(( $a + $b *$c ))
+Dictionary[$i]=$result1
+(( ++i ))
 # Expression2 calculated
-#result2=$(( $a * $b +$c ))
-#echo $result2
-#result3=$(( $c + $a /$b ))
-#echo $result3
-result3=$(( $a % $b +$c ))
-echo $result3
-
+result2=$(( $a * $b +$c ))
+Dictionary[$i]=$result2
+(( ++i ))
+result3=$(( $c + $a /$b ))
+Dictionary[$i]=$result3
+(( ++i ))
+result4=$(( $a % $b +$c ))
+Dictionary[$i]=$result4
+echo "Keys of Dictionary" ${!Dictionary[@]}
+echo "values of Dictionary" ${Dictionary[@]}
