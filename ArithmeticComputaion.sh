cfb1b706e6f310b8f023c642ed321c5da0af04e1
sushmitaku <ksushmita1999@gmail.com>
2020-09-10 10:42:19 +0530

    Arithmetic Expression4 Calculated

--- ArithmeticComputaion.sh
+++ ArithmeticComputaion.sh
@@ -8,6 +8,8 @@ echo "$a  $b  $c "
 # Expression2 calculated
 #result2=$(( $a * $b +$c ))
 #echo $result2
-result3=$(( $c + $a /$b ))
+#result3=$(( $c + $a /$b ))
+#echo $result3
+result3=$(( $a % $b +$c ))
 echo $result3
 
